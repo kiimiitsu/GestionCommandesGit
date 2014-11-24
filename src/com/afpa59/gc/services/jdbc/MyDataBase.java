3a88a96607beabeebabe624f432fd75eeb2e66b6
Caroline C <caroline.chea90@gmail.com>
2014-11-24 12:23:17 +0100

    Création branche JDBC
    ajout class MyDataBase

---
+++ src/com/afpa59/gc/services/jdbc/MyDataBase.java
@@ -0,0 +1,25 @@
+package com.afpa59.gc.services.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDataBase {
+	
+	private static Connection connexion = null;
+	
+	private MyDataBase(){
+		try {
+			Class.forName("org.hsqldb.jdbcDriver");
+			connexion = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/gestioncommandes");
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+	}
+	
+	public static Connection getConnection(){
+		if(connexion==null){
+			new MyDataBase();
+		}
+		return connexion;
+	}
+}
